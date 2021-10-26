0d4ad331d135956bab5aed5e515fd23259ec305d
andrefcpimentel2 <andre@hashicorp.com>
2021-10-26 08:26:55 +0100

    cluster name fix

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ data "vsphere_network" "network" {
 
 resource "vsphere_virtual_machine" "vm" {
   name             = var.vm_name
-  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  resource_pool_id = data.vsphere_compute_cluster.compute_cluster.resource_pool_id
   datastore_id     = data.vsphere_datastore.datastore.id
 
   num_cpus = var.num_cpus
